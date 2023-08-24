cdd7dfb65a765a3bd27bf45f219ff749df92a686
Andrew-R-Y <rakitinandrei@gmail.com>
2023-08-24 21:08:07 +0300

    feat: add Favorites slider functionality

--- library/index.js
+++ library/index.js
@@ -12,20 +12,24 @@ const navMenu = document.querySelector('.header__nav-and-profile');
 document.addEventListener('click', showNav);
 
 function showNav(event) {
-    if (event.target.closest('.header__burger-menu')) {
-        navMenu.classList.toggle('active');
-    }
-    if (!event.target.closest('.header__burger-menu') && !event.target.closest('.header__nav-and-profile') || event.target.closest('.header__item-link')) {
-        navMenu.classList.remove('active');
-    }
-};
+  if (event.target.closest('.header__burger-menu')) {
+    navMenu.classList.toggle('active');
+  }
+  if (
+    (!event.target.closest('.header__burger-menu') &&
+      !event.target.closest('.header__nav-and-profile')) ||
+    event.target.closest('.header__item-link')
+  ) {
+    navMenu.classList.remove('active');
+  }
+}
 
 document.addEventListener('keyup', escapeFunction);
 
 function escapeFunction(event) {
-    if (event.code === 'Escape') {
-        navMenu.classList.remove('active')
-    };
+  if (event.code === 'Escape') {
+    navMenu.classList.remove('active');
+  }
 }
 // Burger menu end
 
@@ -38,173 +42,235 @@ const BUTTON_4 = document.querySelector('.about__pagination-button-4');
 const BUTTON_5 = document.querySelector('.about__pagination-button-5');
 
 BUTTON_1.addEventListener('click', () => {
-    BUTTON_2.classList.remove('about__pagination_active')
-    BUTTON_3.classList.remove('about__pagination_active')
-    BUTTON_4.classList.remove('about__pagination_active')
-    BUTTON_5.classList.remove('about__pagination_active')
-    BUTTON_1.classList.add('about__pagination_active')
-    SLIDER.classList.remove('position-2')
-    SLIDER.classList.remove('position-3')
-    SLIDER.classList.remove('position-4')
-    SLIDER.classList.remove('position-5')
-    SLIDER.classList.add('position-1')
-    LEFT_ARROW.classList.add('arrow-disabled');
-    RIGHT_ARROW.classList.remove('arrow-disabled');
-})
+  BUTTON_2.classList.remove('about__pagination_active');
+  BUTTON_3.classList.remove('about__pagination_active');
+  BUTTON_4.classList.remove('about__pagination_active');
+  BUTTON_5.classList.remove('about__pagination_active');
+  BUTTON_1.classList.add('about__pagination_active');
+  SLIDER.classList.remove('position-2');
+  SLIDER.classList.remove('position-3');
+  SLIDER.classList.remove('position-4');
+  SLIDER.classList.remove('position-5');
+  SLIDER.classList.add('position-1');
+  LEFT_ARROW.classList.add('arrow-disabled');
+  RIGHT_ARROW.classList.remove('arrow-disabled');
+});
 
 BUTTON_2.addEventListener('click', () => {
-    BUTTON_1.classList.remove('about__pagination_active')
-    BUTTON_3.classList.remove('about__pagination_active')
-    BUTTON_4.classList.remove('about__pagination_active')
-    BUTTON_5.classList.remove('about__pagination_active')
-    BUTTON_2.classList.add('about__pagination_active')
-    SLIDER.classList.remove('position-1')
-    SLIDER.classList.remove('position-3')
-    SLIDER.classList.remove('position-4')
-    SLIDER.classList.remove('position-5')
-    SLIDER.classList.add('position-2')
-    LEFT_ARROW.classList.remove('arrow-disabled');
-    RIGHT_ARROW.classList.remove('arrow-disabled');
-})
+  BUTTON_1.classList.remove('about__pagination_active');
+  BUTTON_3.classList.remove('about__pagination_active');
+  BUTTON_4.classList.remove('about__pagination_active');
+  BUTTON_5.classList.remove('about__pagination_active');
+  BUTTON_2.classList.add('about__pagination_active');
+  SLIDER.classList.remove('position-1');
+  SLIDER.classList.remove('position-3');
+  SLIDER.classList.remove('position-4');
+  SLIDER.classList.remove('position-5');
+  SLIDER.classList.add('position-2');
+  LEFT_ARROW.classList.remove('arrow-disabled');
+  RIGHT_ARROW.classList.remove('arrow-disabled');
+});
 
 BUTTON_3.addEventListener('click', () => {
-    BUTTON_1.classList.remove('about__pagination_active')
-    BUTTON_2.classList.remove('about__pagination_active')
-    BUTTON_4.classList.remove('about__pagination_active')
-    BUTTON_5.classList.remove('about__pagination_active')
-    BUTTON_3.classList.add('about__pagination_active')
-    SLIDER.classList.remove('position-1')
-    SLIDER.classList.remove('position-2')
-    SLIDER.classList.remove('position-4')
-    SLIDER.classList.remove('position-5')
-    SLIDER.classList.add('position-3')
-    LEFT_ARROW.classList.remove('arrow-disabled');
-    RIGHT_ARROW.classList.remove('arrow-disabled');
-})
+  BUTTON_1.classList.remove('about__pagination_active');
+  BUTTON_2.classList.remove('about__pagination_active');
+  BUTTON_4.classList.remove('about__pagination_active');
+  BUTTON_5.classList.remove('about__pagination_active');
+  BUTTON_3.classList.add('about__pagination_active');
+  SLIDER.classList.remove('position-1');
+  SLIDER.classList.remove('position-2');
+  SLIDER.classList.remove('position-4');
+  SLIDER.classList.remove('position-5');
+  SLIDER.classList.add('position-3');
+  LEFT_ARROW.classList.remove('arrow-disabled');
+  RIGHT_ARROW.classList.remove('arrow-disabled');
+});
 
 BUTTON_4.addEventListener('click', () => {
-    BUTTON_1.classList.remove('about__pagination_active')
-    BUTTON_2.classList.remove('about__pagination_active')
-    BUTTON_3.classList.remove('about__pagination_active')
-    BUTTON_5.classList.remove('about__pagination_active')
-    BUTTON_4.classList.add('about__pagination_active')
-    SLIDER.classList.remove('position-1')
-    SLIDER.classList.remove('position-2')
-    SLIDER.classList.remove('position-3')
-    SLIDER.classList.remove('position-5')
-    SLIDER.classList.add('position-4')
-    LEFT_ARROW.classList.remove('arrow-disabled');
-    RIGHT_ARROW.classList.remove('arrow-disabled');
-})
+  BUTTON_1.classList.remove('about__pagination_active');
+  BUTTON_2.classList.remove('about__pagination_active');
+  BUTTON_3.classList.remove('about__pagination_active');
+  BUTTON_5.classList.remove('about__pagination_active');
+  BUTTON_4.classList.add('about__pagination_active');
+  SLIDER.classList.remove('position-1');
+  SLIDER.classList.remove('position-2');
+  SLIDER.classList.remove('position-3');
+  SLIDER.classList.remove('position-5');
+  SLIDER.classList.add('position-4');
+  LEFT_ARROW.classList.remove('arrow-disabled');
+  RIGHT_ARROW.classList.remove('arrow-disabled');
+});
 
 BUTTON_5.addEventListener('click', () => {
-    BUTTON_1.classList.remove('about__pagination_active')
-    BUTTON_2.classList.remove('about__pagination_active')
-    BUTTON_3.classList.remove('about__pagination_active')
-    BUTTON_4.classList.remove('about__pagination_active')
-    BUTTON_5.classList.add('about__pagination_active')
-    SLIDER.classList.remove('position-1')
-    SLIDER.classList.remove('position-2')
-    SLIDER.classList.remove('position-3')
-    SLIDER.classList.remove('position-4')
-    SLIDER.classList.add('position-5')
-    LEFT_ARROW.classList.remove('arrow-disabled');
-    RIGHT_ARROW.classList.add('arrow-disabled');
-})
+  BUTTON_1.classList.remove('about__pagination_active');
+  BUTTON_2.classList.remove('about__pagination_active');
+  BUTTON_3.classList.remove('about__pagination_active');
+  BUTTON_4.classList.remove('about__pagination_active');
+  BUTTON_5.classList.add('about__pagination_active');
+  SLIDER.classList.remove('position-1');
+  SLIDER.classList.remove('position-2');
+  SLIDER.classList.remove('position-3');
+  SLIDER.classList.remove('position-4');
+  SLIDER.classList.add('position-5');
+  LEFT_ARROW.classList.remove('arrow-disabled');
+  RIGHT_ARROW.classList.add('arrow-disabled');
+});
 
 const LEFT_ARROW = document.querySelector('.slider-arrow-left');
 const RIGHT_ARROW = document.querySelector('.slider-arrow-right');
 
 LEFT_ARROW.addEventListener('click', () => {
-    if (SLIDER.classList.contains('position-1')) {
-        return
-    } else if (SLIDER.classList.contains('position-2')) {
-        SLIDER.classList.remove('position-2');
-        SLIDER.classList.add('position-1');
-        BUTTON_2.classList.remove('about__pagination_active')
-        BUTTON_1.classList.add('about__pagination_active')
-        LEFT_ARROW.classList.add('arrow-disabled');
-    } else if (SLIDER.classList.contains('position-3')) {
-        SLIDER.classList.remove('position-3');
-        SLIDER.classList.add('position-2');
-        BUTTON_3.classList.remove('about__pagination_active')
-        BUTTON_2.classList.add('about__pagination_active')
-    } else if (SLIDER.classList.contains('position-4')) {
-        SLIDER.classList.remove('position-4');
-        SLIDER.classList.add('position-3');
-        BUTTON_4.classList.remove('about__pagination_active')
-        BUTTON_3.classList.add('about__pagination_active')
-    } else if (SLIDER.classList.contains('position-5')) {
-        SLIDER.classList.remove('position-5');
-        SLIDER.classList.add('position-4');
-        BUTTON_5.classList.remove('about__pagination_active')
-        BUTTON_4.classList.add('about__pagination_active')
-        RIGHT_ARROW.classList.remove('arrow-disabled')
-    }
-})
+  if (SLIDER.classList.contains('position-1')) {
+    return;
+  } else if (SLIDER.classList.contains('position-2')) {
+    SLIDER.classList.remove('position-2');
+    SLIDER.classList.add('position-1');
+    BUTTON_2.classList.remove('about__pagination_active');
+    BUTTON_1.classList.add('about__pagination_active');
+    LEFT_ARROW.classList.add('arrow-disabled');
+  } else if (SLIDER.classList.contains('position-3')) {
+    SLIDER.classList.remove('position-3');
+    SLIDER.classList.add('position-2');
+    BUTTON_3.classList.remove('about__pagination_active');
+    BUTTON_2.classList.add('about__pagination_active');
+  } else if (SLIDER.classList.contains('position-4')) {
+    SLIDER.classList.remove('position-4');
+    SLIDER.classList.add('position-3');
+    BUTTON_4.classList.remove('about__pagination_active');
+    BUTTON_3.classList.add('about__pagination_active');
+  } else if (SLIDER.classList.contains('position-5')) {
+    SLIDER.classList.remove('position-5');
+    SLIDER.classList.add('position-4');
+    BUTTON_5.classList.remove('about__pagination_active');
+    BUTTON_4.classList.add('about__pagination_active');
+    RIGHT_ARROW.classList.remove('arrow-disabled');
+  }
+});
 
 RIGHT_ARROW.addEventListener('click', () => {
-    if (SLIDER.classList.contains('position-5')) {
-        return
-    } else if (SLIDER.classList.contains('position-1')) {
-        SLIDER.classList.remove('position-1');
-        SLIDER.classList.add('position-2');
-        BUTTON_1.classList.remove('about__pagination_active');
-        BUTTON_2.classList.add('about__pagination_active');
-        LEFT_ARROW.classList.remove('arrow-disabled');
-    } else if (SLIDER.classList.contains('position-2')) {
-        SLIDER.classList.remove('position-2');
-        SLIDER.classList.add('position-3');
-        BUTTON_2.classList.remove('about__pagination_active')
-        BUTTON_3.classList.add('about__pagination_active')
-    } else if (SLIDER.classList.contains('position-3')) {
-        SLIDER.classList.remove('position-3');
-        SLIDER.classList.add('position-4');
-        BUTTON_3.classList.remove('about__pagination_active')
-        BUTTON_4.classList.add('about__pagination_active')
-    } else if (SLIDER.classList.contains('position-4')) {
-        SLIDER.classList.remove('position-4');
-        SLIDER.classList.add('position-5');
-        BUTTON_4.classList.remove('about__pagination_active')
-        BUTTON_5.classList.add('about__pagination_active')
-        RIGHT_ARROW.classList.add('arrow-disabled')
-    }
-})
+  if (SLIDER.classList.contains('position-5')) {
+    return;
+  } else if (SLIDER.classList.contains('position-1')) {
+    SLIDER.classList.remove('position-1');
+    SLIDER.classList.add('position-2');
+    BUTTON_1.classList.remove('about__pagination_active');
+    BUTTON_2.classList.add('about__pagination_active');
+    LEFT_ARROW.classList.remove('arrow-disabled');
+  } else if (SLIDER.classList.contains('position-2')) {
+    SLIDER.classList.remove('position-2');
+    SLIDER.classList.add('position-3');
+    BUTTON_2.classList.remove('about__pagination_active');
+    BUTTON_3.classList.add('about__pagination_active');
+  } else if (SLIDER.classList.contains('position-3')) {
+    SLIDER.classList.remove('position-3');
+    SLIDER.classList.add('position-4');
+    BUTTON_3.classList.remove('about__pagination_active');
+    BUTTON_4.classList.add('about__pagination_active');
+  } else if (SLIDER.classList.contains('position-4')) {
+    SLIDER.classList.remove('position-4');
+    SLIDER.classList.add('position-5');
+    BUTTON_4.classList.remove('about__pagination_active');
+    BUTTON_5.classList.add('about__pagination_active');
+    RIGHT_ARROW.classList.add('arrow-disabled');
+  }
+});
 // About slider end
 
 // Favorites seasons selector start
 const SEASONS = document.querySelectorAll('input.favorites__input');
 const BOOKS = document.querySelectorAll('.favorites__book-card');
-let activeSeason;
-let previosSeason = 'winter';
+const BOOKS_CONTAINER = document.querySelector('.favorites__books-container');
+let activeSeason = 'winter';
+let previousSeason;
+BOOKS.forEach((book) => {
+  if (book.classList.contains('winter')) {
+    book.classList.add('is-active');
+  }
+});
+
 SEASONS.forEach(setEventListener);
 
-function setEventListener(item){
-    item.addEventListener('change', showCardsBySeason)
+function setEventListener(item) {
+  item.addEventListener('change', changeSeason);
 }
 
-function showCardsBySeason(event) {
-    activeSeason = event.target.value;
-    console.log('activeSeason:', activeSeason);
-    console.log('previosSeason', previosSeason)
+function changeSeason(event) {
+  BOOKS_CONTAINER.style.height = booksContainerHeight + 'px';
+  activeSeason = event.target.value;
+
+  const removeFadeIn = function () {
+    this.classList.remove('fade-in');
+    this.removeEventListener('animationend', removeFadeIn);
+  };
+
+  const changeActive = function () {
+    this.classList.remove('is-active');
+    this.classList.remove('fade-out');
     BOOKS.forEach((book) => {
-        if(book.classList.contains(activeSeason)) {
-            setTimeout(() => {book.classList.add('favorites__fade-in')}, 2000);
-            setTimeout(() => {book.classList.add('is-active')}, 2000);
-            setTimeout(() => {book.classList.remove('favorites__fade-in')}, 4000);
-        } 
-        if(book.classList.contains(previosSeason)){
-            book.classList.add('favorites__fade-out')
-            setTimeout(() => {book.classList.remove('favorites__fade-out')}, 2000)
-            setTimeout(() => {book.classList.remove('is-active')}, 2000)
-        }
-    })
-    previosSeason = activeSeason;
+      if (book.classList.contains(activeSeason)) {
+        book.classList.add('is-active');
+        book.classList.add('fade-in');
+        book.addEventListener('animationend', removeFadeIn);
+      }
+      this.removeEventListener('animationend', changeActive);
+    });
+  };
+
+  let i = 0;
+
+  BOOKS.forEach((book) => {
+    if (
+      book.classList.contains('is-active') &&
+      book.classList.contains('fade-in')
+    ) {
+      i = 1;
+    } else if (
+      book.classList.contains('is-active') &&
+      book.classList.contains('fade-out')
+    ) {
+      i = 2;
+    } else if (book.classList.contains('is-active')) {
+      i = 3;
+    }
+  });
+
+  BOOKS.forEach((book) => {
+    if (
+      book.classList.contains('is-active') &&
+      book.classList.contains('fade-in')
+    ) {
+      book.classList.remove('is-active');
+      book.classList.remove('fade-in');
+      book.removeEventListener('animationend', removeFadeIn);
+      //
+      //
+    } else if (
+      book.classList.contains('is-active') &&
+      book.classList.contains('fade-out')
+    ) {
+      book.classList.remove('is-active');
+      book.classList.remove('fade-out');
+      book.removeEventListener('animationend', changeActive);
+      //
+      //
+    } else if (book.classList.contains('is-active')) {
+      book.classList.add('fade-out');
+      book.addEventListener('animationend', changeActive);
+    }
+    if (book.classList.contains(activeSeason) && i < 3) {
+      book.classList.add('is-active');
+      book.classList.add('fade-in');
+      book.addEventListener('animationend', removeFadeIn);
+    }
+
+    if (book.classList.contains(previousSeason) && i < 3) {
+      book.classList.remove('fade-in');
+      book.removeEventListener('animationend', removeFadeIn);
+    }
+  });
+  previousSeason = activeSeason;
 }
 
-BOOKS.forEach((book) => {
-    if (book.classList.contains('winter')) {
-        book.classList.add('is-active')
-    }
-})
-// Favorites seasons selector end 
+let booksContainerHeight = BOOKS_CONTAINER.offsetHeight;
+
+// Favorites seasons selector end
