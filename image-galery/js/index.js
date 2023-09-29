83c0e88a3db9c03e8969f2515464debd8f43c830
Andrew-R-Y <rakitinandrei@gmail.com>
2023-09-29 19:19:55 +0300

    feat: add displaying of 15 start-page images, add footer and header

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -1,16 +1,14 @@
 console.log('Hello, Image gallery!');
 
-const IMAGE_CONTAINER = document.querySelector('.wrapper');
+const IMAGE_CONTAINER = document.querySelector('.main');
 url =
-  'https://api.unsplash.com/photos?client_id=kQtp2xCFDX59Fxz8WdVLkEiQ7ryBMrMLY2IQuZ6swRs&per_page=5&page=1';
+  'https://api.unsplash.com/photos?client_id=kQtp2xCFDX59Fxz8WdVLkEiQ7ryBMrMLY2IQuZ6swRs&orientation=landscape&per_page=15&page=1';
 
 async function getPhotos() {
   const result = await fetch(url);
   const photoData = await result.json();
   console.log(photoData);
-  console.log(photoData[1].urls.small);
-  const newImage = createImage(photoData[1].urls.small_s3);
-  IMAGE_CONTAINER.append(newImage);
+  iterateImages(photoData);
 }
 getPhotos();
 
@@ -19,4 +17,18 @@ function createImage(imageUrl) {
   image.src = imageUrl;
   image.alt = 'image';
   return image;
+}
+
+function iterateImages(images) {
+  const infoText = 'images recieved from Unsplash API';
+  if (images.length > 0) {
+    const length = images.length;
+    console.log(`${length} ${infoText}`);
+    for (const image of images) {
+      const newImage = createImage(image.urls.small);
+      IMAGE_CONTAINER.append(newImage);
+    }
+  } else {
+    console.log(`no ${infoText}`);
+  }
 }
