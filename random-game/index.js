b58cba37c01e610075c68d5fe6abeaf2b064a811
Andrew-R-Y <rakitinandrei@gmail.com>
2023-10-14 23:24:32 +0300

    feat: add some styling

--- random-game/index.js
+++ random-game/index.js
@@ -1,1 +1,12 @@
 console.log("Let's go!");
+
+const size = 16;
+let allElements = document.querySelectorAll('.board > span');
+let body = [129, 130, 131];
+console.log(allElements);
+
+function runGame() {
+  for (const element of allElements) {
+    element.classList.remove('body');
+  }
+}
