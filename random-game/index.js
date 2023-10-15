a4f56122f3c205e9e7badb59337c2f0b1f91e758
Andrew-R-Y <rakitinandrei@gmail.com>
2023-10-15 13:41:02 +0300

    add snake moving functionality

--- random-game/index.js
+++ random-game/index.js
@@ -1,17 +1,55 @@
-console.log("Let's go!");
-
+BUTTON = document.querySelector('.button');
 const size = 16;
 let allElements = document.querySelectorAll('.board > span');
-let body = [129, 130, 131];
+let body = [131, 130, 129];
 let result;
+let step = 1;
+let move;
 
 function runGame() {
+  clearInterval(move);
   result = 0;
   for (const element of allElements) {
     element.classList.remove('body');
   }
+  move = setInterval(moving, 250);
 }
 
-body.forEach((piece) => {
-  allElements[piece].classList.add('body');
-});
+function clearBoard() {
+  allElements.forEach((element) => {
+    element.classList.remove('body');
+  });
+}
+
+function moving() {
+  clearBoard();
+  body.unshift(body[0] + step);
+  body.pop();
+  displaySnake();
+}
+
+function displaySnake() {
+  body.forEach((piece) => {
+    allElements[piece].classList.add('body');
+  });
+}
+
+function chooseDirection(event) {
+  if ((event.keyCode === 65 || event.keyCode === 37) && step !== 1) {
+    step = -1;
+  }
+  if ((event.keyCode === 87 || event.keyCode === 38) && step !== 16) {
+    step = -16;
+  }
+  if ((event.keyCode === 68 || event.keyCode === 39) && step !== -1) {
+    step = 1;
+  }
+  if ((event.keyCode === 83 || event.keyCode === 40) && step !== -16) {
+    step = 16;
+  }
+
+  console.log(event.keyCode);
+}
+
+BUTTON.addEventListener('click', runGame);
+document.addEventListener('keyup', chooseDirection);
